a1d9a90a5ec1eeaec351a29c14fe90dc9da46668
Dmitriy Mironov <mironov_2001@mail.ru>
2023-10-28 22:39:08 +0300

    Создал функцию для определения расширения файла

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@
 import argparse
 import sys
 
+from DataReader import DataReader
+
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+from XmlDataReader import XmlDataReader
 
 
 def get_path_from_arguments(args) -> str:
@@ -14,11 +17,20 @@ def get_path_from_arguments(args) -> str:
     args = parser.parse_args(args)
     return args.path
 
+def get_reader(path: str) -> DataReader:
+    file_extension = path.split('.')[-1]
+    print(file_extension)
+    if file_extension == 'txt':
+        return TextDataReader()
+    elif file_extension == 'xml':
+        return XmlDataReader()
+    else:
+        raise ValueError("Расширение файла не поддерживается") 
 
 def main():
     path = get_path_from_arguments(sys.argv[1:])
 
-    reader = TextDataReader()
+    reader = get_reader(path)
     students = reader.read(path)
     print("Students: ", students)
 
